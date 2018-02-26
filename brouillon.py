6692c0da3e223a2920ed5bac8ee049fcf08fd02a
No-Code-Team <swan.blanc@etu.upmc.fr>
2018-02-26 14:55:21 +0100

    Ajoue des lib de base

---
+++ brouillon.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import scipy
+
+from ImgOperator.Filtre.Laplacien import Laplacien
+
+# img = cv2.imread("meme run.png")
+# l = Laplacien()
+# cv2.imshow('Laplacien', l.apply(img))
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    pass
